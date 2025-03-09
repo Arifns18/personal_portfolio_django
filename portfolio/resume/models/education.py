e79a68328e3b1ccc33148ae37b799b1c8d6cbe0e
Arifns18 <73942198+Arifns18@users.noreply.github.com>
2025-03-10 00:28:22 +0800

    Working base portfolio

--- portfolio/resume/models/education.py
+++ portfolio/resume/models/education.py
@@ -1,3 +1,4 @@
+from django.contrib import admin
 from django.db import models
 
 
@@ -6,8 +7,13 @@ class Education(models.Model):
     qualification = models.CharField(max_length=200)
     description = models.CharField(max_length=200)
     cgpa = models.FloatField("CGPA")
+    ongoing = models.BooleanField(default=True)
     start_date = models.DateField("start date")
     end_date = models.DateField("end date", null=True, blank=True)
 
     def __str__(self):
         return self.location
+
+
+class EducationAdmin(admin.ModelAdmin):
+    list_display = ["location", "start_date", "end_date"]
